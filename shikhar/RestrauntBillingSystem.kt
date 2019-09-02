efac9ad2d66ea5594116dbffce497ccc3f582453
Shikhar Uttam <f20190802@pilani.bits-pilani.ac.in>
2019-09-02 18:13:34 +0530

    Update RestrauntBillingSystem.kt
    Added minor changes to the main function to take user input.

--- shikhar/RestrauntBillingSystem.kt
+++ shikhar/RestrauntBillingSystem.kt
@@ -5,33 +5,43 @@
  * He then enters the quantity of the item, and then the item is added to the bill
  * This process is repeated until the waiter has entered all the dishes, which will be communicated by entering a special symbol
  * */
+import java.util.*;
+class RestrauntBillingSystem {
 
-fun main(args: Array<String>) {
+    fun main(args: Array<String>) {
+        val input=Scanner(System.`in`)
+        var category:Int
 
-}
+        println("1-Starters  2-Main Course  3-Desserts  #-Display Bill")
+        category=input.nextInt()
+        showMenuForCategory(category)
 
-/**
- * This function will display the list of items available in the selected category
- * @param category is an integer number that decides the category selected
- * The list of categories is as follows: Starters, Main Course, and Desserts
- * */
-fun showMenuForCategory(category: Int) {
+    }
 
-}
+    /**
+     * This function will display the list of items available in the selected category
+     * @param category is an integer number that decides the category selected
+     * The list of categories is as follows: Starters, Main Course, and Desserts
+     * */
+    fun showMenuForCategory(category: Int) {
 
-/**
- * The work of this function is to add a new item to the bill
- * You are free to decide the types of parameters and return type for this function
- * */
-fun addItemToBill() {
 
-}
+    }
 
-/**
- * This function displays the final bill to the user
- * The bill should contain the name of the restaurant, the items selected, their price, quantity and cost
- * The bill should also display the final amount to be paid
- * */
-fun displayBill() {
+    /**
+     * The work of this function is to add a new item to the bill
+     * You are free to decide the types of parameters and return type for this function
+     * */
+    fun addItemToBill() {
 
+    }
+
+    /**
+     * This function displays the final bill to the user
+     * The bill should contain the name of the restaurant, the items selected, their price, quantity and cost
+     * The bill should also display the final amount to be paid
+     * */
+    fun displayBill() {
+
+    }
 }
